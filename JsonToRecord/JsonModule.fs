58b521e86b7f5cd0b4d14579aa685e7e97b428c7
Rosia <azazf932gmail.com>
2021-01-03 17:24:46 +0300

    Добавьте файлы проекта.

---
+++ JsonToRecord/JsonModule.fs
@@ -0,0 +1,54 @@
+﻿module JsonModule
+open FSharp.Data
+open System
+
+let private tryParseInt (s: decimal) = 
+    try 
+        s.ToString() |> int |> ignore
+        true
+    with :? FormatException -> 
+        false
+
+let private tryParseDateTime (d: string) = 
+    match System.DateTime.TryParse d with
+    | true, _ -> true
+    | _ -> false
+
+let private tryParseGuid (d: string) = 
+    match System.Guid.TryParse d with
+    | true, _ -> true
+    | _ -> false
+
+let rec private jsonValueToTypeString (value: JsonValue) =
+    match value with
+    | JsonValue.Boolean _ -> "bool"
+    | JsonValue.Float _ -> "float"
+    | JsonValue.Array [||] -> "? array"
+    | JsonValue.Array v -> jsonValueToTypeString v.[0] + " array"
+    | JsonValue.Number d -> if tryParseInt d then "int" else "decimal"
+    | JsonValue.String s when tryParseDateTime s -> "DateTime"
+    | JsonValue.String s when tryParseGuid s -> "Guid"
+    | JsonValue.String _ -> "string"
+    | _ -> "?"
+
+let private jsonRecordToRecord (record: (string * JsonValue) [] ) =
+    match record with
+    | [||] -> Error("Json is empty")
+    | _ -> Ok(record |> Array.map(fun (s, t) -> s + ": " + jsonValueToTypeString t + "; ")
+                     |> Array.reduce(fun a t -> a + t))
+    
+let rec private jsonValueToRecord value =
+    match value with
+    | JsonValue.Array arr -> jsonValueToRecord arr.[0]
+    | JsonValue.Record record -> jsonRecordToRecord record
+    | _ -> Error("Error")
+
+let jsonToRecord json name =
+    let info = JsonValue.TryParse(json)
+    match info with
+    | Some v -> 
+        let result =  jsonValueToRecord v
+        match result with
+        | Ok(res) -> "type " + name + " = { " + res + "}"
+        | Error(v) -> v
+    | None -> "Error json parsing"
---
+++ JsonToRecord/Program.fs
@@ -0,0 +1,66 @@
+﻿open Suave.Filters
+open Suave.Operators
+open Suave.Successful
+open Suave
+open Telegram.Bot
+open Telegram.Bot.Types
+open Telegram.Bot.Types.Enums
+open Newtonsoft.Json
+open System
+open NovelFS.NovelIO
+
+
+let client = new TelegramBotClient(Environment.GetEnvironmentVariable("botToken"))
+client.SetWebhookAsync("https://76a49a8113da.ngrok.io/bot").Wait()
+
+let parseUrl (url: string) =
+    try 
+        Some (new Uri(url))
+    with _ -> 
+        None
+
+let parseUrlMessage (mes : string) = 
+    let arr = mes.Split(' ')
+    if arr.Length <> 3 then Error("Error parse message") 
+    else match parseUrl arr.[1] with
+         | Some url -> Ok(url, arr.[2])
+         | None -> Error("Parse url")
+
+let tryGetStringFromUrl (url : Uri) =
+    try 
+        let httpClient = new System.Net.Http.HttpClient()
+        let response = httpClient.GetAsync(url).Result 
+        response.Content.ReadAsStringAsync().Result |> Some
+    with _ -> 
+        None
+
+let sendTextToChat (id : ChatId) (mes : string) = 
+    io { client.SendTextMessageAsync(id, mes).Result |> ignore }
+
+let browse =
+    request (fun r ->
+        let update = r.rawForm |> System.Text.Encoding.Default.GetString |> JsonConvert.DeserializeObject<Update>
+        if update.Message.Type = MessageType.Text then
+            match update.Message.Text with
+            | "/start" -> IO.run (sendTextToChat (new ChatId(update.Message.Chat.Id)) "Bot start")
+            | x when x.StartsWith("/url") -> 
+                  match parseUrlMessage x with
+                  | Result.Ok(url, name) -> 
+                             match tryGetStringFromUrl url with
+                             | Some json -> IO.run (sendTextToChat 
+                                                   (new ChatId(update.Message.Chat.Id)) (JsonModule.jsonToRecord json name))
+                             | None -> IO.run (sendTextToChat (new ChatId(update.Message.Chat.Id)) "Url don't return json")
+                  | Result.Error(e) -> IO.run (sendTextToChat (new ChatId(update.Message.Chat.Id)) e)
+            | _ -> IO.run (sendTextToChat (new ChatId(update.Message.Chat.Id)) "?")
+                     
+        OK "OK" 
+        )
+
+let webPart =    
+    choose [ path "/bot" >=> browse ]
+
+
+[<EntryPoint>]
+let main _ =
+    startWebServer defaultConfig webPart
+    0 
