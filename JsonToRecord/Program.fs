f0f6aaa547bf676d5a52df9a4f37e18ae7051393
Rosia <azazf932gmail.com>
2021-01-11 19:46:08 +0300

    refactor io monad

--- JsonToRecord/Program.fs
+++ JsonToRecord/Program.fs
@@ -11,7 +11,7 @@ open NovelFS.NovelIO
 
 
 let client = new TelegramBotClient(Environment.GetEnvironmentVariable("botToken"))
-client.SetWebhookAsync("https://76a49a8113da.ngrok.io/bot").Wait()
+client.SetWebhookAsync("https://fe1962dca5c2.ngrok.io/bot").Wait()
 
 let parseUrl (url: string) =
     try 
@@ -37,24 +37,26 @@ let tryGetStringFromUrl (url : Uri) =
 let sendTextToChat (id : ChatId) (mes : string) = 
     io { client.SendTextMessageAsync(id, mes).Result |> ignore }
 
+let telegramRequest request = io {
+    let update = request.rawForm |> System.Text.Encoding.Default.GetString |> JsonConvert.DeserializeObject<Update>
+    if update.Message.Type = MessageType.Text then
+        match update.Message.Text with
+        | "/start" -> do! (sendTextToChat (new ChatId(update.Message.Chat.Id)) "Bot start")
+        | x when x.StartsWith("/url") -> 
+              match parseUrlMessage x with
+              | Result.Ok(url, name) -> 
+                         match tryGetStringFromUrl url with
+                         | Some json -> do! (sendTextToChat 
+                                               (new ChatId(update.Message.Chat.Id)) (JsonModule.jsonToRecord json name))
+                         | None -> do! (sendTextToChat (new ChatId(update.Message.Chat.Id)) "Url don't return json")
+              | Result.Error(e) -> do! (sendTextToChat (new ChatId(update.Message.Chat.Id)) e)
+        | _ -> do! (sendTextToChat (new ChatId(update.Message.Chat.Id)) "?")
+                 
+    return OK "OK"
+}
+
 let browse =
-    request (fun r ->
-        let update = r.rawForm |> System.Text.Encoding.Default.GetString |> JsonConvert.DeserializeObject<Update>
-        if update.Message.Type = MessageType.Text then
-            match update.Message.Text with
-            | "/start" -> IO.run (sendTextToChat (new ChatId(update.Message.Chat.Id)) "Bot start")
-            | x when x.StartsWith("/url") -> 
-                  match parseUrlMessage x with
-                  | Result.Ok(url, name) -> 
-                             match tryGetStringFromUrl url with
-                             | Some json -> IO.run (sendTextToChat 
-                                                   (new ChatId(update.Message.Chat.Id)) (JsonModule.jsonToRecord json name))
-                             | None -> IO.run (sendTextToChat (new ChatId(update.Message.Chat.Id)) "Url don't return json")
-                  | Result.Error(e) -> IO.run (sendTextToChat (new ChatId(update.Message.Chat.Id)) e)
-            | _ -> IO.run (sendTextToChat (new ChatId(update.Message.Chat.Id)) "?")
-                     
-        OK "OK" 
-        )
+    request (fun r -> IO.run(telegramRequest r))
 
 let webPart =    
     choose [ path "/bot" >=> browse ]
